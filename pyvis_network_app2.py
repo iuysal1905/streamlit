69d9dbc59c241ba6e9ea20f68a7dc2d37ebf6079
ilhan uysal <63537319+iuysal1905@users.noreply.github.com>
2023-01-31 16:31:20 +0300

    Update pyvis_network_app2.py

--- pyvis_network_app2.py
+++ pyvis_network_app2.py
@@ -6,7 +6,7 @@ from pyvis.network import Network
 import pip
 pip.main(["install", "openpyxl"])
 
-# Read dataset (CSV)
+# Read dataset 
 df_interact = pd.read_excel('data1.xlsx')
 
 # Set header title
@@ -26,12 +26,12 @@ if len(selected_drugs) == 0:
 
 # Create network graph when user selects >= 1 item
 else:
-    df_select = df_interact.loc[df_interact['drug_1_name'].isin(selected_drugs) | \
-                                df_interact['drug_2_name'].isin(selected_drugs)]
+    df_select = df_interact.loc[df_interact['drug1_name'].isin(selected_drugs) | \
+                                df_interact['drug2_name'].isin(selected_drugs)]
     df_select = df_select.reset_index(drop=True)
 
     # Create networkx graph object from pandas dataframe
-    G = nx.from_pandas_edgelist(df_select, 'drug_1_name', 'drug_2_name', 'weight')
+    G = nx.from_pandas_edgelist(df_select, 'drug1_name', 'drug2_name', 'weight')
 
     # Initiate PyVis network object
     drug_net = Network(
@@ -72,8 +72,9 @@ else:
 st.markdown(
     """
     <br>
-    <h6><a href="https://github.com/kennethleungty/Pyvis-Network-Graph-Streamlit" target="_blank">GitHub Repo</a></h6>
-    <h6><a href="https://kennethleungty.medium.com" target="_blank">Medium article</a></h6>
+    <h6><a href="https://iuysal1905-streamlit-networkapp-kztp4y.streamlit.app/" target="_blank">Network 1</a></h6>
+    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-serum-y63ywu.streamlit.app/" target="_blank">Network 2</a></h6>
+    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-73qax4.streamlit.app/" target="_blank">Network 3</a></h6>
     <h6>Disclaimer: This app is NOT intended to provide any form of medical advice or recommendations. Please consult your doctor or pharmacist for professional advice relating to any drug therapy.</h6>
     """, unsafe_allow_html=True
     )
